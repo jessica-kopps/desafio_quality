f4fbaced7dff0ad6558b7d58eaec88d003764a36
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-03-31 15:44:14 -0300

    new test to throw an exception when the Neighborhood does not exist

--- desafio_quality/src/test/java/desafio_quality/desafio_quality/service/PropertyTest.java
+++ desafio_quality/src/test/java/desafio_quality/desafio_quality/service/PropertyTest.java
@@ -1,4 +1,6 @@
 package desafio_quality.desafio_quality.service;
+
+import desafio_quality.desafio_quality.exception.NeighborhoodDoesntExists;
 import desafio_quality.desafio_quality.factory.PropertyFactory;
 import desafio_quality.desafio_quality.model.Neighborhood;
 import desafio_quality.desafio_quality.model.Property;
@@ -14,10 +16,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -33,16 +37,22 @@ public class PropertyTest {
     private PropertyService service;
 
 
-    private void setupFindBy(){
+    private void setupFindBy() {
         Property property = PropertyFactory.createProperty();
         Mockito.when(repository.findById(Mockito.any())).thenReturn(property);
     }
 
-    private void setupFindAll(){
+    private void setupFindAll() {
         Property property = PropertyFactory.createProperty();
         Mockito.when(neighborhoodRepository.findAll()).thenReturn(Arrays.asList(new Neighborhood(property.getNeighborhood().getName(),
                 property.getNeighborhood().getValueDistrictM2())));
         Mockito.when(repository.insert(Mockito.any())).thenReturn(property);
+    }
+
+    private void setupFindAllThrowsException(){
+        Property property = PropertyFactory.createProperty();
+        Mockito.when(neighborhoodRepository.findAll()).thenReturn(Arrays.asList(new Neighborhood(property.getNeighborhood().getName(),
+                property.getNeighborhood().getValueDistrictM2())));
     }
 
     @Test
@@ -62,7 +72,7 @@ public class PropertyTest {
     }
 
     @Test
-    void shouldCreatePropertyWhenNeighborhoodExists () {
+    void shouldCreatePropertyWhenNeighborhoodExists() {
         this.setupFindAll();
         Property property = PropertyFactory.createProperty();
         Property createProperty = service.createProperty(property);
@@ -71,21 +81,22 @@ public class PropertyTest {
 
 
     @Test
-    void shouldThrowsExceptionWhenNeighborhoodDoesntExists () {
-
+    void shouldThrowsExceptionWhenNeighborhoodDoesntExists() throws Exception {
+        this.setupFindAllThrowsException();
+        Property property = PropertyFactory.createProperty();
+        property.setNeighborhood(new Neighborhood("NomeDoBairro", BigDecimal.valueOf(666.00)));
+        assertThrows(NeighborhoodDoesntExists.class, () -> {
+            service.createProperty(property);
+        });
     }
 
     @Test
-    void shouldCalculateCorrectRoomArea(){
+    void shouldCalculateCorrectRoomArea() {
         this.setupFindAll();
         Property property = PropertyFactory.createProperty();
         Property createProperty = service.createProperty(property);
         assertEquals(createProperty.getRooms().get(0).getArea(), property.getRooms().get(0).calculateArea());
     }
-
-
-
-
 
 
 }
